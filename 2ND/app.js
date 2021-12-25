624316806b6a1900d75b062b3a3dc06409bfd867
minju1009 <90900882+minju1009@users.noreply.github.com>
2021-12-25 23:09:43 +0900

    JS: login

--- 2ND/app.js
+++ 2ND/app.js
@@ -1,4 +1,15 @@
-const loginForm = document.querySelector(#login-form);
+const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 
+function onLoginBtnClick() {
+    const username = loginInput.value;
+    if(username === ""){
+        console.log("Please write your name");
+    }else if(username.length > 15) {
+        alert("Your name is too long");
+    }
+
+}
+
+loginButton.addEventListener("click", onLoginBtnClick)
