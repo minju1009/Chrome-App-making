57fb4261fca054054c743a663c0d7b86ccb966ab
minju1009 <90900882+minju1009@users.noreply.github.com>
2021-12-30 23:23:06 +0900

    JS: login complete

--- 2ND/app.js
+++ 2ND/app.js
@@ -3,18 +3,25 @@ const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
     greeting.innerText = `hello ${username}`
     greeting.classList.remove(HIDDEN_CLASSNAME);
     
     console.log(username);
 }
 
-
-
 loginForm.addEventListener("submit", onLoginSubmit);
 
+const savedUsernmae = localStorage.getItem(USERNAME_KEY);
+
+if( savedUsernmae === null) {
+
+}else {
+
+}
